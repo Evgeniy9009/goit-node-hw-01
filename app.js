1d0b7f9f1608ae6d0accef46ad9a5c8c5c5c9b7b
Evgeniy9009 <evgeniy.grabar1@gmail.com>
2022-11-30 22:29:47 +0200

    add commander

--- app.js
+++ app.js
@@ -1,44 +1,67 @@
+const { Command } = require("commander");
+const program = new Command()
+
 // const nodemon = require("nodemon")
 const contacts = require('./contacts')
 
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+console.log(argv)
+
 const invokeAction = async ({ action, id, name, email, phone }) => {
     switch(action) {
-        case 'listContacts':
+        case 'list':
             const allContacts = await contacts.listContacts();
-            console.log(allContacts);
+            console.table(allContacts);
             break;
-        case 'getContactById':
+        case 'get':
             const oneContact = await contacts.getContactById(id);
             console.log(oneContact);
             break;
-        case 'removeContact':
+        case 'remove':
             const deleteContact = await contacts.removeContact(id);
             console.log(deleteContact);
             break;
-        case 'addContact':
+        case 'add':
             const newContact = await contacts.addContact({ name, email, phone });
             console.log(newContact);
             break;
-        case 'updateById':
+        case 'update':
             const updateContact = await contacts.updateById(id, { name, email, phone });
             console.log(updateContact);
             break;
-        default:console.log('Unknown action')
-        
+        default:
+            await console.warn("\x1B[31m Unknown action type!");
+            // console.log('Unknown action')
     }
 }
-// invokeAction({ action: 'listContacts' })
-// invokeAction({ action: 'getContactById', id: '3' })
+
+invokeAction(argv);
+
+// invokeAction({ action: 'list' })
+// invokeAction({ action: 'get', id: '3' })
 // invokeAction({
-//     action: 'addContact',
+//     action: 'add',
 //     name: 'Evgeniy',
 //     email: 'evgeniy.gmail.com',
 //     phone: '(098) 2434-177'})
-// invokeAction({ action: 'removeContact', id: 'd061559f-1c48-44bc-8f22-c0ed8aeaadc1'})
+// invokeAction({ action: 'remove', id: 'd061559f-1c48-44bc-8f22-c0ed8aeaadc1'})
 
 // invokeAction({
-//     action: 'updateById',
+//     action: 'update',
 //     id: 'd061559f-1c48-44bc-8f22-c0ed8aeaadc1',
 //     name: 'Evgeniy1111',
 //     email: 'evgeniy111.gmail.com',
 //     phone: '(098) 1111-177'})
+
+// node app.js --action get --id 5
+// node app.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+// node app.js --action remove --id=10
